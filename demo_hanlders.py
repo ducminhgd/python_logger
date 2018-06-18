c375bbd45ab6c08903dc3555496e4410e1af9a0f
MinhGDD <minh.gdd@teko.vn>
2018-06-18 17:28:44 +0700

    Exception stack trace

--- demo_hanlders.py
+++ demo_hanlders.py
@@ -1,5 +1,29 @@
+import traceback
+
 import requests
 from logging.handlers import HTTPHandler
+
+
+def format_stack_trace(exc_info):
+    """
+    Format exception info as string
+    :param exc_info: exception info
+    :return: string
+    :example:
+
+    import sys
+
+    try:
+        ...
+    except:
+        exc_info = sys.exc_info()
+        print(format_stack_trace(exc_info))
+    """
+    if exc_info[0] is None:
+        return ''
+    lines = traceback.format_exception(*exc_info)
+    return ''.join(line for line in lines)
+
 
 class PersistentHTTPHandler(HTTPHandler):
     def __init__(self, logPath, host, url, method):
@@ -11,6 +35,11 @@ class PersistentHTTPHandler(HTTPHandler):
         record_modified = HTTPHandler.mapLogRecord(self, record)
         record_modified['logPath'] = self.logPath
         record_modified['msg'] = (record_modified['msg'] % record_modified['args']).encode('utf-8')
+        if record_modified['exc_info'] is not None:
+            record_modified['msg'] = '\n'.join([
+                record_modified['msg'],
+                format_stack_trace(record_modified['exc_info'])
+            ])
         return record_modified
 
     def emit(self, record):
