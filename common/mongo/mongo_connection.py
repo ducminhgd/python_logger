557046a0a9d2ea44633ea7a5a44241f22b9c547f
vchitai <tai.vc@teko.vn>
2018-07-24 14:02:00 +0700

    Fix mini bugs

--- common/mongo/mongo_connection.py
+++ common/mongo/mongo_connection.py
@@ -15,7 +15,7 @@ class MongoConnectionBase(object):
                 self._connections[key] = MongoClient(host=host, port=port, connect=False)
 
     def get_connection(self, host, port):
-        key = host + port
+        key = host + ":" + port
         if key not in self._connections.keys():
             self._connections[key] = MongoClient(host=host, port=int(port), connect=False)
         return self._connections[key]
