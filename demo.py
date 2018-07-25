ffcd15f9e5913c074de3217bfd54cb55ee5e766a
vchitai <tai.vc@teko.vn>
2018-07-25 09:03:33 +0700

    Update Mongo Logger

--- demo.py
+++ demo.py
@@ -8,16 +8,27 @@ LOGGING = {
     'handlers': {
         'test': {
             'level': 'INFO',
-            'class': 'demo_hanlders.PersistentHTTPHandler',
+            'class': 'demo_handlers.PersistentHTTPHandler',
             'url': 'http-daily-logger/',
             'host': 'localhost:1234',
             'method': 'POST',
             'logPath': '/tmp/python_logger/log'
         },
+        'test_mongo': {
+            'level': 'INFO',
+            'class': 'demo_handlers.MongoHTTPHandler',
+            'url': 'http-mongo-logger/',
+            'host': 'localhost:1234',
+            'method': 'POST',
+            'db_host': '127.0.0.1',
+            'db_port': '27017',
+            'db_name': 'logs',
+            'collection': 'logs',
+        }
     },
     'loggers': {
         'test_logger': {
-            'handlers': ['test'],
+            'handlers': ['test_mongo'],
             'level': 'INFO',
             'propagate': True,
         },
@@ -28,7 +39,12 @@ logging.config.dictConfig(LOGGING)
 
 if __name__ == '__main__':
     logger = logging.getLogger('test_logger')
-    start = time.time()
-    for i in range(0, 1):
-        logger.info('1' * 100)
-    print(time.time() - start)
+    try:
+        a = 1/0
+    except:
+        logger.exception('error')
+    # start = time.time()
+    # for i in range(0, 1):
+    #     logger.info('1' * 100)
+    #
+    # print(time.time() - start)
--- demo_handlers.py
+++ demo_handlers.py
@@ -1,51 +1,9 @@
-# Python logger
-
-
-## Sample configuration
-
-### Client Side
-```python
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'test': {
-            'level': 'INFO',
-            'class': 'demo_hanlders.PersistentHTTPHandler',
-            'url': 'http-daily-logger/',
-            'host': 'localhost:1234',
-            'method': 'POST',
-            'logPath': '/tmp/python_logger/log'
-        },
-        'test_mongo': {
-            'level': 'INFO',
-            'class': 'demo_handlers.MongoHTTPHandler',
-            'url': 'http-mongo-logger/',
-            'host': 'localhost:1234',
-            'method': 'POST',
-            'db_host': '127.0.0.1',
-            'db_port': '27017',
-            'db_name': 'logs',
-            'collection': 'logs',
-        }
-    },
-    'loggers': {
-        'test_logger': {
-            'handlers': ['test', 'test_mongo'],
-            'level': 'INFO',
-            'propagate': True,
-        },
-    }
-}
-```
-
-## Sample Log Hanlder
-
-### File HTTP Handler
-
-```python
+import json
 import requests
 from logging.handlers import HTTPHandler
+
+from common.message_utils import format_exc_info, ExtendedJsonEncoder
+
 
 class PersistentHTTPHandler(HTTPHandler):
     def __init__(self, logPath, host, url, method):
@@ -56,7 +14,10 @@ class PersistentHTTPHandler(HTTPHandler):
     def mapLogRecord(self, record):
         record_modified = HTTPHandler.mapLogRecord(self, record)
         record_modified['logPath'] = self.logPath
-        record_modified['msg'] = (record_modified['msg'] % record_modified['args']).encode('utf-8')
+        try:
+            record_modified['msg'] = self.format(record)
+        except:
+            pass
         return record_modified
 
     def emit(self, record):
@@ -71,11 +32,8 @@ class PersistentHTTPHandler(HTTPHandler):
             raise
         except:
             self.handleError(record)
-```
 
-### Mongo HTTP Handler
 
-```python
 class MongoHTTPHandler(HTTPHandler):
     def __init__(self, db_host, db_port, db_name, collection, host, url, method):
         HTTPHandler.__init__(self, host, url, method)
@@ -115,39 +73,3 @@ class MongoHTTPHandler(HTTPHandler):
             raise
         except:
             self.handleError(record)
-```
-
-## Run
-
-### With Gunicorn
-
-```bash
-gunicorn python_logger.wsgi:application -k gevent -w 4 -b 127.0.0.1:1234 --log-file /var/log/python_logger/gunicorn.log --keep-alive 5 --graceful-timeout 60 --pid /tmp/python-logger.pid
-```
-
-### With SystemD
-
-```bash
-# python_logger.service
-[Unit]
-Description=python_logger
-After=network.target
-
-[Service]
-PIDFile=/tmp/python_logger.pid
-User=root
-Group=root
-WorkingDirectory=/data/python_logger
-ExecStart=/data/python_logger/.venv/bin/gunicorn python_logger.wsgi:application -k gevent -w 4 -b 127.0.0.1:1234 --log-file /var/log/python_logger/gunicorn.log --keep-alive 5 --graceful-timeout 60 --pid /tmp/python-logger.pid
-ExecReload = /bin/kill -s HUP $MAINPID
-ExecStop = /bin/kill -s TERM $MAINPID
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
-```
-
-## Contributors
-
-- ducminhgd
-- vchitai
