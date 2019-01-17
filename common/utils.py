b94753e4beb7f18acab701d34e273b61b686f17d
thao.dt <thao.dt@teko.vn>
2019-01-17 16:36:16 +0700

    update util func

--- common/utils.py
+++ common/utils.py
@@ -29,10 +29,13 @@ def format_stack_trace(exc_info):
     lines = traceback.format_exception(*exc_info)
     return ''.join(line for line in lines)
 
-def cleanup(day_keep_alive, log_dir):
+def cleanup(day_keep_alive, file_name):
+    log_dir = os.path.dirname(file_name)
     current_time = time.time()
     day_conversion_unit = 24 * 3600
     for f in os.listdir(log_dir):
+        if not f.endswith('.log'):
+            continue
         file_path = os.path.join(log_dir, f)
         modified_time = os.path.getctime(file_path)
         if (current_time - modified_time) // day_conversion_unit <= day_keep_alive:
